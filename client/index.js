237c918e26816dc5c6a89d0880be11019f18a757
a-baade <andreasbaade@hotmail.com>
2022-02-17 11:28:43 +0100

    minimal server functionality

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+
 ReactDOM.render(
   <h1>Welcome to the quiz broadcast and remain indoors</h1>,
   document.getElementById("app")
--- server/server.js
+++ server/server.js
@@ -3,13 +3,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/login", (req, res, next) => {
-  res.json({ username: "Noen André Persson" });
-});
-app.post("/login", (req, res, next) => {
-  res.sendStatus(401);
-});
-
 app.use(express.static("../client/dist"));
 
 const server = app.listen(process.env.PORT || 3000, () => {
