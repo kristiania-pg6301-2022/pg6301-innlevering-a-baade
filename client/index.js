7cb84ecba020cda4abec1035c68c872235d8ea5c
a-baade <andreasbaade@hotmail.com>
2022-02-17 13:24:03 +0100

    minimal server functionality

--- client/index.js
+++ client/index.js
@@ -1,8 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
 
+
+export function Frontpage() {
+    return <div>
+        <h1>Quiz Broadcast</h1>
+        <Link to={"question"}>
+                <button>Remain indoors</button>
+        </Link>
+    </div>;
+}
+
+function Application() {
+    return (
+        <BrowserRouter>
+        <Routes>
+            <Route path = {"/"} element = {<Frontpage/>} />
+        </Routes>
+    </BrowserRouter>
+    );
+}
 
 ReactDOM.render(
-  <h1>Welcome to the quiz broadcast and remain indoors</h1>,
+  <Application/>,
   document.getElementById("app")
 );
