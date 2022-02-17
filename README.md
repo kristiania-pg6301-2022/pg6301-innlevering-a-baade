9453ab27ac2a2973c1e209e60b8dc16f54ca5584
a-baade <andreasbaade@hotmail.com>
2022-02-17 13:52:37 +0100

    Husky pre-commit added

--- README.md
+++ README.md
@@ -10,20 +10,25 @@
 - [x] Create client side files
 - [x] `npm init`/client
 - [x] npm install/client
-    - `npm i -D parcel jest prettier babel-jest "@babel/preset-env`
+    - `npm i -D parcel jest babel-jest "@babel/preset-env`
     - `npm i -P react react-dom react-router-dom`
 - [x] Create server side files
 - [x] `npm init`/server
 - [x] npm install/server
     - `npm i -D express body-parser`
     - `npm i -P nodemon`
-
+##Husky
+- [x] Install Husky 
+  - `npm i -D husky`
+  - `npx husky install`
+  - `npm set-script prepare "husky install"`
+  - `npx husky add .husky/pre-commit "npm run format:check"`
 ## **Route Requests**
-- [ ] Routing requests in app
+- [x] Routing requests in app
     - Wrap application in `<BrowserRouter`
     - Wrap route sets in `<Routes>`
     - Wrap "destinations" in `<Route path="..." element={<SomeElement />} />`
     - Route with `<Link to="...">`
 
 ## List questions
-- [ ] import *quiz.js* 
+- [x] import *quiz.js* 
--- client/index.js
+++ client/index.js
@@ -1,28 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Link, Routes, Route} from "react-router-dom";
-
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
 
 export function Frontpage() {
-    return <div>
-        <h1>Quiz Broadcast</h1>
-        <Link to={"question"}>
-                <button>Remain indoors</button>
-        </Link>
-    </div>;
+  return (
+    <div>
+      <h1>Quiz Broadcast</h1>
+      <Link to={"question"}>
+        <button>Remain indoors</button>
+      </Link>
+    </div>
+  );
 }
 
 function Application() {
-    return (
-        <BrowserRouter>
-        <Routes>
-            <Route path = {"/"} element = {<Frontpage/>} />
-        </Routes>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<Frontpage />} />
+      </Routes>
     </BrowserRouter>
-    );
+  );
 }
 
-ReactDOM.render(
-  <Application/>,
-  document.getElementById("app")
-);
+ReactDOM.render(<Application />, document.getElementById("app"));
