cc7e6128e89210e5d3be8442b4477e7d15127af7
a-baade <andreasbaade@hotmail.com>
2022-04-05 17:02:09 +0200

    make it better

--- server/server.js
+++ server/server.js
@@ -59,6 +59,6 @@ app.use((req, res, next) => {
   }
 });
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`server started at http://localhost:${server.address().port}`);
 });
